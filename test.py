17e7e822dc905accd3397411ac31172d131a9514
Pootares Ketsaart <s6901012610188@email.kmutnb.ac.th>
2026-08-02 19:53:57 +0700

    add num4

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 num1 = input("Enter the first number: ")
 num2 = input("Enter the second number: ")
 num3 = input("Enter the third number: ")
+num4 = input("Enter the fourth number: ")
 
-sum = float(num1) + float(num2) + float(num3)
+sum = float(num1) + float(num2) + float(num3) + float(num4)
 print("The sum is:", sum)
