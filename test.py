995b0c65c8d1a9aed5ca5b9629b1b72fe32a2746
Pootares Ketsaart <s6901012610188@email.kmutnb.ac.th>
2026-08-02 19:52:17 +0700

    create test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+num3 = input("Enter the third number: ")
+
+sum = float(num1) + float(num2) + float(num3)
+print("The sum is:", sum)
